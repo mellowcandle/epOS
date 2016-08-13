46dc56a93c3ccdd9af516b9e651f1c305e768079
Ramon Fried <ramon.fried@gmail.com>
2016-08-13 23:42:37 +0300

    scripts: add -d option to start_new_kernel.sh to start in debug mode
    Signed-off-by: Ramon Fried <ramon.fried@gmail.com>

--- start_new_kernel.sh
+++ start_new_kernel.sh
@@ -1,3 +1,25 @@
-#!/bin/bash
+#!/bin/bash 
+
+DEBUG=0
+while [[ $# -gt 0 ]]
+do
+key="$1"
+
+case $key in
+    -d|--debug)
+    DEBUG=1
+    shift # past argument
+    ;;
+    *)
+            # unknown option
+    ;;
+esac
+shift # past argument or value
+done
+
+if [[ $DEBUG -eq 1 ]]; then
+	qemu-system-i386 -s -S -cdrom kernel.iso
+else
+	qemu-system-i386 -cdrom kernel.iso
+fi
 #qemu-system-i386 -kernel ./kernel.bin
-qemu-system-i386 -cdrom kernel.iso
