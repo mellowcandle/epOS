443c8aaa796eaca5d7a70a3ce0ddad47ee041236
Ramon Fried <ramon.fried@gmail.com>
2016-09-21 02:37:45 +0300

    Remove trailing spaces from assembly
    Signed-off-by: Ramon Fried <ramon.fried@gmail.com>

--- boot/loader.s
+++ boot/loader.s
@@ -28,7 +28,7 @@ extern kmain                            ; _main is defined elsewhere
 global mbd ; Grub structure
 global magic ; Grub magic number
 global pdt; Page directory
-global pte; 
+global pte;
 ; setting up the Multiboot header - see GRUB docs for details
 MODULEALIGN equ  1<<0             ; align loaded modules on page boundaries
 MEMINFO     equ  1<<1             ; provide memory map
@@ -36,20 +36,20 @@ FLAGS       equ  MODULEALIGN | MEMINFO  ; this is the Multiboot 'flag' field
 MAGIC       equ  0x1BADB002     ; 'magic number' lets bootloader find the header
 CHECKSUM    equ -(MAGIC + FLAGS)  ; checksum required
 STACKSIZE	equ 0x4000
- 
+
 ; This is the virtual base address of kernel space. It must be used to convert virtual
 ; addresses into physical addresses until paging is enabled. Note that this is not
 ; the virtual address where the kernel image itself is loaded -- just the amount that must
 ; be subtracted from a virtual address to get a physical address.
 KERNEL_VIRTUAL_BASE equ 0xC0000000                  ; 3GB
 KERNEL_PAGE_NUMBER equ (KERNEL_VIRTUAL_BASE >> 22)  ; Page directory index of kernel's 4MB PTE.
- 
+
 section .multiboot
 align 4
     dd MAGIC
     dd FLAGS
     dd CHECKSUM
- 
+
 
 section     .bootcode
 global      EntryPoint
@@ -59,22 +59,22 @@ EntryPoint:
     ; addresses, not virtual ones!
     mov ecx, (pdt - KERNEL_VIRTUAL_BASE)
     mov cr3, ecx                                        ; Load Page Directory Base Register.
- 
+
     mov ecx, cr4
     or ecx, 0x00000010                          ; Set PSE bit in CR4 to enable 4MB pages.
     mov cr4, ecx
- 
+
     mov ecx, cr0
     or ecx, 0x80000000                          ; Set PG bit in CR0 to enable paging.
     mov cr0, ecx
- 
+
     ; Start fetching instructions in kernel space.
     ; Since eip at this point holds the physical address of this command (approximately 0x00100000)
     ; we need to do a long jump to the correct virtual address of StartInHigherHalf which is
     ; approximately 0xC0100000.
     lea ecx, [StartInHigherHalf]
     jmp ecx                                                     ; NOTE: Must be absolute jump!
-  
+
 
 section .data
 align 4096
@@ -91,20 +91,20 @@ pdt:
     ; This page directory entry defines a 4MB page containing the kernel.
     dd 0x00000083
     times (1024 - KERNEL_PAGE_NUMBER - 1) dd 0  ; Pages after the kernel image.
- 
- 
+
+
 section .text
 align 4
 
 ; reserve initial kernel stack space -- that's 16k.
- 
+
 
 StartInHigherHalf:
     ; Unmap the identity-mapped first 4MB of physical address space. It should not be needed
     ; anymore.
     mov dword [pdt], 0
     invlpg [0]
- 
+
     ; NOTE: From now on, paging should be enabled. The first 4MB of physical address space is
     ; mapped starting at KERNEL_VIRTUAL_BASE. Everything is linked to this address, so no more
     ; position-independent code or funny business with virtual-to-physical address translation
@@ -112,16 +112,16 @@ StartInHigherHalf:
     mov esp, stack+STACKSIZE           ; set up the stack
 
 	mov [magic], eax
- 
+
     ; pass Multiboot info structure -- WARNING: This is a physical address and may not be
     ; in the first 4MB!
 
-	add ebx, KERNEL_VIRTUAL_BASE 
-	mov [mbd], ebx 
+	add ebx, KERNEL_VIRTUAL_BASE
+	mov [mbd], ebx
     call  kmain                  ; call kernel proper
     hlt                          ; halt machine should kernel return
- 
- 
+
+
 
 ; This will set up our new segment registers. We need to do
 ; something special in order to set CS. We do what is called a
