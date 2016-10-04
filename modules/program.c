905751a4946d6a63c2590366dea28fd14c38a8bb
Ramon Fried <ramon.fried@gmail.com>
2016-10-04 23:25:13 +0300

    modules compilation - early beginning of user space
    Signed-off-by: Ramon Fried <ramon.fried@gmail.com>

---
+++ modules/program.c
@@ -0,0 +1,10 @@
+int main(int argc, char *argv[])
+{
+
+	here:
+	__asm(".intel_syntax noprefix");
+
+	__asm volatile ("mov eax, 0xdeadbeef");
+	goto here;
+	return 0;
+}
---
+++ modules/start.s
@@ -0,0 +1,8 @@
+extern main
+
+section .text
+	; push argv
+	; push argc
+	call main
+	; main has returned, eax is return value
+	jmp  $    ; loop forever
