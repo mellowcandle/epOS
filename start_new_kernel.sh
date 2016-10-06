3f6e94ede311f9a7caa53e53d087d614d888ee73
Ramon Fried <ramon.fried@gmail.com>
2016-10-06 16:33:12 +0300

    start qemu in non-graphic mode
    Signed-off-by: Ramon Fried <ramon.fried@gmail.com>

--- start_new_kernel.sh
+++ start_new_kernel.sh
@@ -30,7 +30,7 @@ done
 if [[ $DEBUG -eq 1 ]]; then
 	qemu-system-i386 -m 1G,slots=3,maxmem=4G -s -S -cdrom kernel.iso
 else
-	qemu-system-i386 -m 1G,slots=3,maxmem=4G -cdrom kernel.iso
+	qemu-system-i386 -nographic -m 1G,slots=3,maxmem=4G -cdrom kernel.iso
 fi
 
 #qemu-system-i386 -kernel ./kernel.bin
