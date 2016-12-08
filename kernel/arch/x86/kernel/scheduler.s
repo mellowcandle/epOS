ef04fc14a102136367be9063508b882aa1047498
RamonX Fried <ramonx.fried@intel.com>
2016-12-08 20:58:11 +0200

    Added switch to task assembly stuff
    Signed-off-by: RamonX Fried <ramonx.fried@intel.com>

--- kernel/arch/x86/kernel/scheduler.s
+++ kernel/arch/x86/kernel/scheduler.s
@@ -1,14 +1,40 @@
-[bits 32]
+;
+;	This is free and unencumbered software released into the public domain.
+;
+;	Anyone is free to copy, modify, publish, use, compile, sell, or
+;	distribute this software, either in source code form or as a compiled
+;	binary, for any purpose, commercial or non-commercial, and by any
+;	means.
+;
+;	In jurisdictions that recognize copyright laws, the author or authors
+;	of this software dedicate any and all copyright interest in the
+;	software to the public domain. We make this dedication for the benefit
+;	of the public at large and to the detriment of our heirs and
+;	successors. We intend this dedication to be an overt act of
+;	relinquishment in perpetuity of all present and future rights to this
+;	software under copyright law.
+;
+;	THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+;	EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+;	MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+;	IN NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY CLAIM, DAMAGES OR
+;	OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+;	ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+;	OTHER DEALINGS IN THE SOFTWARE.
+;
+;	For more information, please refer to <http://unlicense.org>
+;
 
 section .text
 align 4
+
 global run_kernel_task
+global run_user_task
 
 run_kernel_task:
-    cli                             ; disable external interrupts
-    mov     eax, [esp+4]            ; load address of registers_t into eax
+	cli
+	mov		eax, [eax+4] ; load task_register_t pointer to eax
 
-    ; restore all the registers except eax
     mov     ebx, [eax+4]
     mov     ecx, [eax+8]
     mov     edx, [eax+12]
@@ -28,4 +54,4 @@ run_kernel_task:
 
     iret                            ; iret to return to the process
 
-
+run_user_task:
