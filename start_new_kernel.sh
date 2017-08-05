31f82fac91c43fb0a82605792d4ada81c2809150
Ramon Fried <ramon.fried@gmail.com>
2017-08-05 04:16:38 +0300

    scripts: add traces to qemu
    Signed-off-by: Ramon Fried <ramon.fried@gmail.com>

--- start_new_kernel.sh
+++ start_new_kernel.sh
@@ -3,6 +3,7 @@
 DEBUG=0
 QEMU=0
 OPTIONS="-nographic -m 1G,slots=3,maxmem=4G -cdrom kernel.iso"
+TRACES="-d guest_errors"
 while [[ $# -gt 0 ]]
 do
 key="$1"
@@ -30,5 +31,5 @@ done
 if [[ $DEBUG -eq 1 ]]; then
 	qemu-system-i386 ${OPTIONS} -s -S 2>&1 | tee run.log
 else
-	qemu-system-i386 ${OPTIONS} 2>&1 | tee run.log
+	qemu-system-i386 ${OPTIONS} ${TRACES} 2>&1 | tee run.log
 fi
