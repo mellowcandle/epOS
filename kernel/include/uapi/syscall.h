849fc644aff39a9ec846c4f4a7870a8fadbfa66a
Ramon Fried <ramon.fried@gmail.com>
2017-08-10 16:37:01 +0300

    added uapi file with syscalls
    Signed-off-by: Ramon Fried <ramon.fried@gmail.com>

---
+++ kernel/include/uapi/syscall.h
@@ -0,0 +1,94 @@
+/*
+	This is free and unencumbered software released into the public domain.
+
+	Anyone is free to copy, modify, publish, use, compile, sell, or
+	distribute this software, either in source code form or as a compiled
+	binary, for any purpose, commercial or non-commercial, and by any
+	means.
+
+	In jurisdictions that recognize copyright laws, the author or authors
+	of this software dedicate any and all copyright interest in the
+	software to the public domain. We make this dedication for the benefit
+	of the public at large and to the detriment of our heirs and
+	successors. We intend this dedication to be an overt act of
+	relinquishment in perpetuity of all present and future rights to this
+	software under copyright law.
+
+	THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+	EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+	MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+	IN NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY CLAIM, DAMAGES OR
+	OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+	ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+	OTHER DEALINGS IN THE SOFTWARE.
+
+	For more information, please refer to <http://unlicense.org>
+*/
+#ifndef SYSCALL_H_M6WW51RJ
+#define SYSCALL_H_M6WW51RJ
+
+void init_syscalls();
+
+#define DECL_SYSCALL0(fn) int syscall_##fn();
+#define DECL_SYSCALL1(fn,p1) int syscall_##fn(p1);
+#define DECL_SYSCALL2(fn,p1,p2) int syscall_##fn(p1,p2);
+#define DECL_SYSCALL3(fn,p1,p2,p3) int syscall_##fn(p1,p2,p3);
+#define DECL_SYSCALL4(fn,p1,p2,p3,p4) int syscall_##fn(p1,p2,p3,p4);
+#define DECL_SYSCALL5(fn,p1,p2,p3,p4,p5) int syscall_##fn(p1,p2,p3,p4,p5);
+
+#define DEFN_SYSCALL0(fn, num) \
+int syscall_##fn() \
+{ \
+  int a; \
+  asm volatile("int $0x80" : "=a" (a) : "0" (num)); \
+  return a; \
+}
+
+#define DEFN_SYSCALL1(fn, num, P1) \
+int syscall_##fn(P1 p1) \
+{ \
+  int a; \
+  asm volatile("int $0x80" : "=a" (a) : "0" (num), "b" ((int)p1)); \
+  return a; \
+}
+
+#define DEFN_SYSCALL2(fn, num, P1, P2) \
+int syscall_##fn(P1 p1, P2 p2) \
+{ \
+  int a; \
+  asm volatile("int $0x80" : "=a" (a) : "0" (num), "b" ((int)p1), "c" ((int)p2)); \
+  return a; \
+}
+
+#define DEFN_SYSCALL3(fn, num, P1, P2, P3) \
+int syscall_##fn(P1 p1, P2 p2, P3 p3) \
+{ \
+  int a; \
+  asm volatile("int $0x80" : "=a" (a) : "0" (num), "b" ((int)p1), "c" ((int)p2), "d"((int)p3)); \
+  return a; \
+}
+
+#define DEFN_SYSCALL4(fn, num, P1, P2, P3, P4) \
+int syscall_##fn(P1 p1, P2 p2, P3 p3, P4 p4) \
+{ \
+  int a; \
+  asm volatile("int $0x80" : "=a" (a) : "0" (num), "b" ((int)p1), "c" ((int)p2), "d" ((int)p3), "S" ((int)p4)); \
+  return a; \
+}
+
+#define DEFN_SYSCALL5(fn, num) \
+int syscall_##fn(P1 p1, P2 p2, P3 p3, P4 p4, P5 p5) \
+{ \
+  int a; \
+  asm volatile("int $0x80" : "=a" (a) : "0" (num), "b" ((int)p1), "c" ((int)p2), "d" ((int)p3), "S" ((int)p4), "D" ((int)p5)); \
+  return a; \
+}
+
+
+DECL_SYSCALL1(open, const char * file)
+DECL_SYSCALL1(close, int)
+DECL_SYSCALL3(read, int, char *, int)
+DECL_SYSCALL3(write, int, const char *, int)
+
+
+#endif /* end of include guard: SYSCALL_H_M6WW51RJ */
