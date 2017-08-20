2f40d56f65b3261cdfc185afb3fa85d847444fcd
Ramon Fried <ramon.fried@gmail.com>
2017-08-20 15:08:35 +0300

    modules: program now uses syscalls
    Signed-off-by: Ramon Fried <ramon.fried@gmail.com>

--- modules/program.c
+++ modules/program.c
@@ -3,37 +3,24 @@
 
 int printf(const char *format, ...);
 
-int stdin;
-int stdout;
-int stderr;
+static int __stdin;
+static int __stdout;
+static int __stderr;
+
+void stdlib_init()
+{
+	__stdin = syscall_open("/dev/keyboard");
+	__stdout = syscall_open("/dev/console1");
+	__stderr = syscall_open("/dev/console2");
+}
+
 
 //int main(int argc, char *argv[])
 int main()
 {
-	int ret;
-	stdin = syscall_open("/dev/console");
-	ret = syscall_close(stdin);
-//	stdout = syscall_open("/dev/console");
-//	stderr = syscall_open("/dev/console");
-
-
-//	unsigned int i=0;
-	while (1) ;
+	while (1)
 	{
-
-		//		__asm volatile("hlt");
-	//__asm(".intel_syntax noprefix");
-	__asm("int $0x80");
-
-//		__asm volatile("mov eax, 0xdeadbeef");
-//		printf("1 - %d\r\n",i++);
 	}
 
-#if 0
-here:
-
-	__asm volatile("mov eax, 0xdeadbeef");
-	goto here;
-#endif
 	return 0;
 }
