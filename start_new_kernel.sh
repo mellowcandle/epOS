a409ea3b24098d0a41e3a64d808198adb6d3423f
Ramon Fried <ramon.fried@gmail.com>
2017-08-20 13:34:00 +0300

    scripts: start_new_kernel: enable graphics
    Signed-off-by: Ramon Fried <ramon.fried@gmail.com>

--- start_new_kernel.sh
+++ start_new_kernel.sh
@@ -2,7 +2,7 @@
 
 DEBUG=0
 QEMU=0
-OPTIONS="-nographic -m 1G,slots=3,maxmem=4G -cdrom kernel.iso"
+GFX="-nographic"
 TRACES="-d guest_errors"
 while [[ $# -gt 0 ]]
 do
@@ -21,12 +21,18 @@ case $key in
 	simics/simics simics/targets/x58-ich10/x58-ich10-dvdboot.simics
 	exit $?
 	;;
+	-g|--graphics)
+	GFX=
+	shift
+	;;
 	*)
             # unknown option
     ;;
 esac
 shift # past argument or value
 done
+
+OPTIONS="${GFX} -m 1G,slots=3,maxmem=4G -cdrom kernel.iso"
 
 if [[ $DEBUG -eq 1 ]]; then
 	qemu-system-i386 ${OPTIONS} -s -S 2>&1 | tee run.log
