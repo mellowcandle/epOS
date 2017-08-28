4e80f759cfa601378d35ac65008c2d131c799d3b
Ramon Fried <ramon.fried@gmail.com>
2017-08-28 07:01:57 +0300

    ramfs: kernel implementation
    Signed-off-by: Ramon Fried <ramon.fried@gmail.com>

---
+++ kernel/include/tar.h
@@ -0,0 +1,98 @@
+/*
+	This is free and unencumbered software released into the public domain.
+
+	Anyone is free to copy, modify, publish, use, compile, sell, or
+	distribute this software, either in source code form or as a compiled
+	binary, for any purpose, commercial or non-commercial, and by any
+	means.
+
+	In jurisdictions that recognize copyright laws, the author or authors
+	of this software dedicate any and all copyright interest in the
+	software to the public domain. We make this dedication for the benefit
+	of the public at large and to the detriment of our heirs and
+	successors. We intend this dedication to be an overt act of
+	relinquishment in perpetuity of all present and future rights to this
+	software under copyright law.
+
+	THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+	EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+	MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+	IN NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY CLAIM, DAMAGES OR
+	OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+	ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+	OTHER DEALINGS IN THE SOFTWARE.
+
+	For more information, please refer to <http://unlicense.org>
+*/
+#ifndef TAR_H_FB4PXOUE
+#define TAR_H_FB4PXOUE
+
+/*
+ * Standard Archive Format - Standard TAR - USTAR
+ */
+#define  RECORDSIZE  512
+#define  NAMSIZ      100
+#define  TUNMLEN      32
+#define  TGNMLEN      32
+
+typedef union
+{
+	char	charptr[RECORDSIZE];
+	struct header
+	{
+		char	name[NAMSIZ];
+		char	mode[8];
+		char	uid[8];
+		char	gid[8];
+		char	size[12];
+		char	mtime[12];
+		char	chksum[8];
+		char	linkflag;
+		char	linkname[NAMSIZ];
+		char	magic[8];
+		char	uname[TUNMLEN];
+		char	gname[TGNMLEN];
+		char	devmajor[8];
+		char	devminor[8];
+	} header;
+} tar_header_t;
+
+/* The checksum field is filled with this while the checksum is computed. */
+#define    CHKBLANKS    "        "        /* 8 blanks, no null */
+
+/* The magic field is filled with this if uname and gname are valid. */
+#define    TMAGIC    "ustar  "        /* 7 chars and a null */
+
+/* The magic field is filled with this if this is a GNU format dump entry */
+#define    GNUMAGIC  "GNUtar "        /* 7 chars and a null */
+
+/* The linkflag defines the type of file */
+#define  LF_OLDNORMAL '\0'       /* Normal disk file, Unix compatible */
+#define  LF_NORMAL    '0'        /* Normal disk file */
+#define  LF_LINK      '1'        /* Link to previously dumped file */
+#define  LF_SYMLINK   '2'        /* Symbolic link */
+#define  LF_CHR       '3'        /* Character special file */
+#define  LF_BLK       '4'        /* Block special file */
+#define  LF_DIR       '5'        /* Directory */
+#define  LF_FIFO      '6'        /* FIFO special file */
+#define  LF_CONTIG    '7'        /* Contiguous file */
+
+/* Further link types may be defined later. */
+
+/* Bits used in the mode field - values in octal */
+#define  TSUID    04000        /* Set UID on execution */
+#define  TSGID    02000        /* Set GID on execution */
+#define  TSVTX    01000        /* Save text (sticky bit) */
+
+/* File permissions */
+#define  TUREAD   00400        /* read by owner */
+#define  TUWRITE  00200        /* write by owner */
+#define  TUEXEC   00100        /* execute/search by owner */
+#define  TGREAD   00040        /* read by group */
+#define  TGWRITE  00020        /* write by group */
+#define  TGEXEC   00010        /* execute/search by group */
+#define  TOREAD   00004        /* read by other */
+#define  TOWRITE  00002        /* write by other */
+#define  TOEXEC   00001        /* execute/search by other */
+
+#endif /* end of include guard: TAR_H_FB4PXOUE */
---
+++ kernel/kernel/ramfs.c
@@ -0,0 +1,113 @@
+/*
+	This is free and unencumbered software released into the public domain.
+
+	Anyone is free to copy, modify, publish, use, compile, sell, or
+	distribute this software, either in source code form or as a compiled
+	binary, for any purpose, commercial or non-commercial, and by any
+	means.
+
+	In jurisdictions that recognize copyright laws, the author or authors
+	of this software dedicate any and all copyright interest in the
+	software to the public domain. We make this dedication for the benefit
+	of the public at large and to the detriment of our heirs and
+	successors. We intend this dedication to be an overt act of
+	relinquishment in perpetuity of all present and future rights to this
+	software under copyright law.
+
+	THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+	EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+	MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+	IN NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY CLAIM, DAMAGES OR
+	OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+	ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+	OTHER DEALINGS IN THE SOFTWARE.
+
+	For more information, please refer to <http://unlicense.org>
+*/
+#define DEBUG
+
+#include <tar.h>
+#include <lib/list.h>
+#include <kmalloc.h>
+#include <printk.h>
+#define TAR_BLOCK_SIZE 512
+#define BLOCK_MASK (~(TAR_BLOCK_SIZE-1))
+#define BLOCK_ALIGN_UP(_x) (((_x)+TAR_BLOCK_SIZE - 1) & BLOCK_MASK)
+
+
+static LIST(ramfs_headers);
+typedef struct
+{
+	tar_header_t *ptr;
+	list_t list_head;
+} ramfs_node;
+
+static uint32_t atoi(const char *in, uint32_t len)
+{
+
+	FUNC_ENTER();
+	uint32_t size = 0;
+	uint32_t count = 1;
+
+	for (; len > 0; len--, count *= 8)
+	{
+		size += ((in[len - 1] - '0') * count);
+	}
+
+	return size;
+
+}
+int parse_ramfs(void *ramfs, uint32_t size)
+{
+	FUNC_ENTER();
+	uint32_t offset = 0;
+	uint32_t adapt;
+	ramfs_node *new_header;
+	uint8_t *pos;
+
+	while (size)
+	{
+		pos = (uint8_t *) ramfs + offset;
+
+		if (pos[0] == 0) // Detect EOF
+		{
+			break;
+		}
+
+		new_header = kmalloc(sizeof(ramfs_node));
+		assert(new_header);
+		new_header->ptr = (tar_header_t *)pos;
+
+		adapt = TAR_BLOCK_SIZE +
+		        BLOCK_ALIGN_UP(atoi(new_header->ptr->header.size, 11));
+		size -= adapt;
+		offset += adapt;
+
+		list_add(&new_header->list_head, &ramfs_headers);
+		pr_debug("RAMFS Entry added: %c %s\r\n", new_header->ptr->header.linkflag,
+		         new_header->ptr->header.name);
+	}
+
+	return 0;
+}
+
+int init_ramfs(addr_t phy_addr, uint32_t len)
+{
+	FUNC_ENTER();
+	pr_debug("phy_addr: 0x%x len: 0x%x\r\n", phy_addr, len);
+	uint32_t pagecnt = divide_up(len, PAGE_SIZE);
+	addr_t aligned_addr = PAGE_ALIGN_DOWN(phy_addr);
+
+	void *ramfs = mem_page_map_kernel(aligned_addr, pagecnt, READ_WRITE_KERNEL);
+
+	if (!ramfs)
+	{
+		pr_fatal("Can't map memory\r\n");
+		panic();
+	}
+
+	pr_info("RAMFS: 0x%x->0x%x\r\n", phy_addr, (addr_t) ramfs);
+
+	ramfs += (phy_addr - aligned_addr);
+	return parse_ramfs(ramfs, len);
+}
